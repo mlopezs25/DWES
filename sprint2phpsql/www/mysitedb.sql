737348983a177fc9827cb1d6d411a0f9170b1129
mlopezs25 <mlopezs24@fpcoruna.afundacion.org>
2025-10-23 12:12:23 +0200

    Ejercicio 6

---
+++ sprint2phpsql/www/mysitedb.sql
@@ -0,0 +1,161 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.8.3-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mysitedb
+-- ------------------------------------------------------
+-- Server version	11.8.3-MariaDB-0+deb13u1 from Debian
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `comentarios`
+--
+
+DROP TABLE IF EXISTS `comentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comentarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `elemento_id` int(11) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `comentario` text NOT NULL,
+  `fecha` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentarios`
+--
+
+LOCK TABLES `comentarios` WRITE;
+/*!40000 ALTER TABLE `comentarios` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `comentarios` VALUES
+(1,2,'MANUEL','BIEN','2025-10-23 10:01:32'),
+(2,2,'nbkjnd','holaaaaaaaa','2025-10-23 10:01:32'),
+(3,2,'hoy','ahora','2025-10-23 10:01:32');
+/*!40000 ALTER TABLE `comentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `tComentarios`
+--
+
+DROP TABLE IF EXISTS `tComentarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tComentarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(2000) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `libro_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  KEY `libro_id` (`libro_id`),
+  CONSTRAINT `tComentarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `tUsuarios` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `tComentarios_ibfk_2` FOREIGN KEY (`libro_id`) REFERENCES `tLibros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tComentarios`
+--
+
+LOCK TABLES `tComentarios` WRITE;
+/*!40000 ALTER TABLE `tComentarios` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tComentarios` VALUES
+(1,'Me encantó este libro, una obra maestra.',1,1),
+(2,'Una lectura imprescindible de la literatura española.',2,2),
+(3,'Me encantó este libro, una obra maestra.',1,1),
+(4,'Una lectura imprescindible de la literatura española.',2,2);
+/*!40000 ALTER TABLE `tComentarios` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `tLibros`
+--
+
+DROP TABLE IF EXISTS `tLibros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tLibros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `url_imagen` varchar(200) DEFAULT NULL,
+  `autor` varchar(100) NOT NULL,
+  `anio_publicacion` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tLibros`
+--
+
+LOCK TABLES `tLibros` WRITE;
+/*!40000 ALTER TABLE `tLibros` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tLibros` VALUES
+(1,'Cien Años de Soledad','https://example.com/cien_anos.jpg','Gabriel García Márquez',1967),
+(2,'Don Quijote de la Mancha','https://example.com/don_quijote.jpg','Miguel de Cervantes',1605),
+(3,'Cien Años de Soledad','https://example.com/cien_anos.jpg','Gabriel García Márquez',1967),
+(4,'Don Quijote de la Mancha','https://example.com/don_quijote.jpg','Miguel de Cervantes',1605);
+/*!40000 ALTER TABLE `tLibros` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `tUsuarios`
+--
+
+DROP TABLE IF EXISTS `tUsuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tUsuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contraseña` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tUsuarios`
+--
+
+LOCK TABLES `tUsuarios` WRITE;
+/*!40000 ALTER TABLE `tUsuarios` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tUsuarios` VALUES
+(1,'Juan','Pérez Gómez','juan.perez@example.com','1234'),
+(2,'María','López Fernández','maria.lopez@example.com','abcd');
+/*!40000 ALTER TABLE `tUsuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
+
+-- Dump completed on 2025-10-23 12:04:30
